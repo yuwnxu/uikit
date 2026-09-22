e398ad5a16e0bcd184267309a4473f1eaef2cd62
yuwnxu <yulduzgebekova@yandex.ru>
2026-09-22 14:01:55 +0500

    Добавлены шрифты

--- lib/typography.dart
+++ lib/typography.dart
@@ -3,8 +3,8 @@ import 'package:flutter/material.dart';
 import 'color.dart';
 
 // Основная типографика проекта
-final screenHeader = TextStyle(fontFamily: 'Manrope', fontWeight: .w700, fontSize: 24.fh, color: black, letterSpacing: -0.6, height: 32.fh);
-final TextStyle subHeader = TextStyle(fontFamily: 'Manrope', letterSpacing: -0.18, height: 24.fh, fontWeight: FontWeight.w600, fontSize: 18.fh, color: black);
-final TextStyle bodyMedium = TextStyle(fontFamily: 'Manrope', letterSpacing: 0, height: 24.fh, fontWeight: FontWeight.w400, fontSize: 16.fh, color: black);
-final TextStyle bodySmall = TextStyle(fontFamily: 'Manrope', letterSpacing: 0, height: 20.fh, fontWeight: FontWeight.w400, fontSize: 14.fh, color: black);
-final TextStyle fieldLabel = TextStyle(fontFamily: 'Manrope', letterSpacing: 0, height: 20.fh, fontWeight: FontWeight.w600, fontSize: 14.fh, color: black);
+final TextStyle screenHeader = TextStyle(fontFamily: 'Manrope', fontWeight: FontWeight.w700, fontSize: 24.fh, color: black, letterSpacing: -0.6, height: 32.fh / 24.fh);
+final TextStyle subHeader = TextStyle(fontFamily: 'Manrope', letterSpacing: -0.18, height: 24.fh / 18.fh, fontWeight: FontWeight.w600, fontSize: 18.fh, color: black);
+final TextStyle bodyMedium = TextStyle(fontFamily: 'Manrope', letterSpacing: 0, height: 24.fh / 16.fh, fontWeight: FontWeight.w400, fontSize: 16.fh, color: black);
+final TextStyle bodySmall = TextStyle(fontFamily: 'Manrope', letterSpacing: 0, height: 20.fh / 14.fh, fontWeight: FontWeight.w400, fontSize: 14.fh, color: black);
+final TextStyle fieldLabel = TextStyle(fontFamily: 'Manrope', letterSpacing: 0, height: 20.fh / 14.fh, fontWeight: FontWeight.w600, fontSize: 14.fh, color: black);
